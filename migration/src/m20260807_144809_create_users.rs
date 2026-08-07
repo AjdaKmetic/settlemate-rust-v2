540985c2a5ec66b4bb560b2cca723f1c669940f5
Ajda Kmetič <kmetic.ajda@gmail.com>
2026-08-07 17:28:47 +0200

    Dodana tabela users in SeaORM entiteta

---
+++ migration/src/m20260807_144809_create_users.rs
@@ -0,0 +1,50 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Users::Id))
+                    .col(string(Users::Name).not_null())
+                    .col(string(Users::Username).not_null().unique_key())
+                    .col(string(Users::Email).not_null().unique_key())
+                    .col(string(Users::PasswordHash).not_null())
+                    .col(
+                        timestamp(Users::CreatedAt)
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    Name,
+    Username,
+    Email,
+    PasswordHash,
+    CreatedAt,
+}
